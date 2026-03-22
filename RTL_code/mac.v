adf057aecbfbc5edc18c941079d37f6e7bda5460
KPYerawar <krushnayerawar2004@gmail.com>
2026-03-22 08:36:27 +0530

    mac code cmpleted

--- RTL_code/mac.v
+++ RTL_code/mac.v
@@ -6,6 +6,19 @@ output reg [7:0] a_out ,
 output reg [7:0] b_out ,
 output reg [15:0] sum_out );
 
-
+always @(posedge clk ) begin 
+  if (reset )  begin 
+   a_out<= 8'b0;
+   b_out <= 8'b0;
+   sum_out <= 16'b0 ;
+    end 
+    
+    else begin 
+    a_out<= a_in ;
+    b_out <= b_in ;
+    sum_out <= sum_out + ( a_in * b_in ) ;
+    end 
+    end 
+    endmodule 
 
 
