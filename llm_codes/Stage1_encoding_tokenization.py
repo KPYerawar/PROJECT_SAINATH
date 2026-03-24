2df04b95f5440d70fdd8ecde41765b2daea3605d
Krushna Yerawar <krushnayerawar2004@gmail.com>
2026-03-24 21:55:20 +0530

    Implement SimpleTokenizerV2 with encoding and decoding
    Added a new tokenizer class and updated encoding/decoding methods.

--- llm_codes/Stage1_encoding_tokenization.py
+++ llm_codes/Stage1_encoding_tokenization.py
@@ -68,3 +68,61 @@ class SimpleTokenizerV1:
         text = " ".join([self.int_to_str[i] for i in ids])
         text = re.sub(r'\s+([<>/?.\'"()|\;:])', r'\1', text)
         return text
+
+tokenizer = SimpleTokenizerV1(vocab)
+
+text = """"It's the last he painted, you know,"
+           Mrs. Gisburn said with pardonable pride."""
+ids = tokenizer.encode(text)
+print(ids)
+tokenizer.decode(ids)
+text = "Hello, do you like tea?"
+print(tokenizer.encode(text))
+
+
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+
+vocab = {token:integer for integer,token in enumerate(all_tokens)}
+
+
+  len(vocab.items())
+
+for i, item in enumerate(list(vocab.items())[-5:]):
+    print(item)
+
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = { i:s for s,i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [
+            item if item in self.str_to_int
+            else "<|unk|>" for item in preprocessed
+        ]
+
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
+        return text
+
+tokenizer = SimpleTokenizerV2(vocab)
+
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+
+text = " <|endoftext|> ".join((text1, text2))
+
+print(text)
+
+tokenizer.encode(text)
+
+tokenizer.decode(tokenizer.encode(text))
+
